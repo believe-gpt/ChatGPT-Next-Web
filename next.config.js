1f3e6a98d22cbf1b17fe7095a8a06c81e2fcd915
想当当 <5918760@gmail.com>
2023-05-02 14:30:16 +0800

    WIP:

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  assetPrefix: process.env.NODE_ENV === "production" ? "https://chat-gpt-next-web-three-nu-19.vercel.app/" : "",
   experimental: {
     appDir: true,
   },
